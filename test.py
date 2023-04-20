39f4d7874fed3c44d0c18e3a0f527d665f830ff8
SergeyMorin <serega-morin@yandex.ru>
2023-04-20 21:23:53 +0300

    task cw

--- test.py
+++ test.py
@@ -52,5 +52,29 @@ print(b)
 # b = list(filter(lambda x: x%10 == 5, a))
 # print(b)
 
-zip()
-b = list(enumerate())
+# zip()
+# b = list(enumerate())
+
+# ////////////////////////////////////////////////////////////////////////////////////////////
+
+# файлы 
+# a - append - дописывает или создает 
+# r - read - чтение 
+# w - write - перезапись
+# w+ - запись и чтение, создание
+# r+ - чтение и добавление
+
+colors = ['red', 'green', 'blue']
+data = open('file.txt', 'a')
+data.writelines(colors)
+data.close()
+
+with open('file.txt', 'w') as data:
+    data.write('line 1\n')
+    data.write('line 2\n')
+
+path = 'file.txt'
+data = open('file.txt', 'r')
+for line in data:
+    print(line)
+data.close()
